eece7cce1bc5f69b2fb6f39df3a844dfed0793fd
Samuel Rembisz <sjrembisz07@gmail.com>
2021-10-04 19:03:20 -0400

    Finished employment section

---
+++ resume/src/components/past_employment/past_employment.js
@@ -0,0 +1,48 @@
+
+export const employment_arr = [
+    {
+        name: "uBreakiFix By Asurion",
+        url: "https://www.ubreakifix.com/locations/fredericksburg?utm_campaign=fredericksburg",
+        role: "Computer and Phone Repair Technician",
+        time_started: new Date('May 1, 2019 00:00:00'),
+        time_ended: new Date('July 1, 2021 00:00:00'),
+    },
+    {
+        name: "Riverbend Highschool CS/Math Department",
+        url: "https://www.spotsylvania.k12.va.us/Domain/35",
+        role: "Volunteer Mentor and Teacher Aid",
+        time_started: new Date('Jan 1, 2018 00:00:00'),
+        time_ended: null,
+    },
+    {
+        name: "Community Ministry Center",
+        url: "http://communityministrycenter.org/",
+        role: "Volunteer IT Consultant",
+        time_started: new Date('Jan 1, 2018 00:00:00'),
+        time_ended: new Date('August 1, 2018 00:00:00'),
+    },
+]
+
+export const education_arr = [
+    {
+        establishment: "George Mason University",
+        title: "Master of Computer Science (MCS)",
+        time_started: new Date('August 1, 2020'),
+        time_ended: new Date('May 1, 2024')
+    },
+    {
+        establishment: "Riverbend Highschool",
+        title: "Advanced High School Diploma",
+        time_started: new Date('August 1, 2016'),
+        time_ended: new Date('May 1, 2020')
+    },
+]
+
+export const honors_arr = [
+    "Dean's List Fall 2020",
+    "Vice President and Secretary of the Zeta Omicron National Computer Honors Society",
+    "Co-Leader of the Tech Discussion Club",
+    "Leader of the 1st place team at the 2016 Capital One Hacker-thon",
+    "Winner of the VCU 2018 Hackathon",
+    "Apart of the winning team of the UMW 2019 Hackathon"
+]
--- resume/src/components/past_employment/past_employment.vue
+++ resume/src/components/past_employment/past_employment.vue
@@ -2,21 +2,91 @@
   <div class="past_employment_section">
     <div class="past_employment_empty"></div>
     <transition name="past_employment_text" appear>
-        <div class="past_employment_box">
-            <div class="past_employment_content">
-                <div class="work_experience content">
-                    <h1>Work Experience</h1>
-                    <div class="work_list_item"><p><a href="https://www.ubreakifix.com/">uBreakiFix</a></p> <p>May 2019 - July 2021</p></div>
+      <div class="past_employment_box">
+        <div class="past_employment_content">
+          <div
+            class="work_experience content"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <h1>Work Experience</h1>
+            <div
+              v-for="employment in employment_arr"
+              :key="employment.name"
+              class="work_list_item"
+            >
+              <div class="work_left">
+                <div class="name">
+                  <p>
+                    <a v-bind:href="employment.url">{{ employment.name }}</a>
+                  </p>
                 </div>
-                <div class="education content">
-
+                <div class="date">
+                  <p>
+                    <template v-if="employment.time_started">
+                      {{ dateFormat(employment.time_started, "mmmm yyyy") }}
+                    </template>
+                    <template v-else>Present</template>
+                    -
+                    <template v-if="employment.time_ended">
+                      {{ dateFormat(employment.time_ended, "mmmm yyyy") }}
+                    </template>
+                    <template v-else>Present</template>
+                  </p>
                 </div>
-                <div class="honors content">
-
-                </div>
+              </div>
+              <div class="work_right">
+                <p>{{ employment.role }}</p>
+              </div>
             </div>
-            <div class="past_employment_empty"></div>
+          </div>
+          <div
+            class="education content"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <h1>Education</h1>
+            <div
+              v-for="education in education_arr"
+              :key="education.establishment"
+              class="work_list_item"
+            >
+              <div class="work_left">
+                <div class="name">
+                  <p>{{ education.establishment }}</p>
+                </div>
+                <div class="date">
+                  <p>
+                    <template v-if="education.time_started">
+                      {{ dateFormat(education.time_started, "mmmm yyyy") }}
+                    </template>
+                    <template v-else>Present</template>
+                    -
+                    <template v-if="education.time_ended">
+                      {{ dateFormat(education.time_ended, "mmmm yyyy") }}
+                    </template>
+                    <template v-else>Present</template>
+                  </p>
+                </div>
+              </div>
+              <div class="work_right">
+                <p>{{ education.title }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="honors content" data-aos="fade-up" data-aos-delay="600">
+            <h1>Honors</h1>
+            <div class="honors_list">
+              <ul v-for="honor in honors_arr" :key="honor">
+                <li>
+                  <p>{{ honor }}</p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+        <!-- <div class="past_employment_empty"></div> -->
+      </div>
     </transition>
     <!-- <div class="about_me_empty"></div> -->
   </div>
@@ -25,11 +95,18 @@
 <script>
 import AOS from "aos";
 import "aos/dist/aos.css";
+import {
+  employment_arr,
+  education_arr,
+  honors_arr,
+} from "./past_employment.js";
+import dateFormat from "dateformat";
 
 export default {
   name: "past_employment",
   setup: () => {
     AOS.init();
+    return { employment_arr, education_arr, honors_arr, dateFormat };
   },
 };
 </script>
@@ -49,7 +126,7 @@ export default {
     var(--solid-background) 100%
   );
   margin: 0px 0px 0px 0px;
-  height: 100vh;
+  height: 80vh;
 }
 
 .past_employment_empty {
@@ -57,17 +134,42 @@ export default {
 }
 
 .past_employment_content {
-    display:inline-flex;
+  display: inline-flex;
 }
 
 .content {
-    text-align: center;
-    width: 33vw;
+  text-align: center;
+  width: 33vw;
 }
 
 .work_list_item {
-    display: inline-flex;
-    justify-content: space-between;
+  display: inline-flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.honors_list {
+  text-align: center;
+}
+
+.honors_list > ul {
+  display: inline-flex;
+}
+
+.work_left {
+  display: table;
+  width: 50%;
+}
+
+.work_right {
+  display: inline-block;
+  width: 50%;
+  padding: auto;
+  margin: auto;
+}
+
+li {
+  color: white;
 }
 
 h1 {
@@ -76,6 +178,11 @@ h1 {
 }
 
 p {
+  color: white;
+  font-family: "Monospace";
+}
+
+a {
   color: white;
   font-family: "Monospace";
 }
