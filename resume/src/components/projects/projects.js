ec2a6b67ecb4a6e3ce2337e7186a909c4c36f254
Samuel Rembisz <sjrembisz07@gmail.com>
2022-03-15 03:08:37 -0400

    Added GvG to resume

--- resume/src/components/projects/projects.js
+++ resume/src/components/projects/projects.js
@@ -1,49 +1,52 @@
-
 export const proj_arr = [
-    {
-        name: "BRShtop",
-        url: "https://github.com/StappsWorld/brshtop",
-        tools: ["rust"],
-        description: "<u><strong>A Rusty implementation of BpyTop</strong></u>, a stronger, more informative Top written in Python. I ported this program to Rust as a means of immersing myself further the language. It is fully memory safe.",
-        loc: 50000,
-        time_started: new Date('November 1, 2020 00:00:00'),
-        time_ended: new Date('March 1, 2021 00:00:00'),
-    },
-    {
-        name: "YALAL",
-        url: "https://github.com/StappsWorld/YALAL",
-        tools: ["rust"],
-        description: "<u><strong>A simple linear algebra library</strong></u> written in Rust with N dimensional vectors and matrices. Included with the library is unit testing to verify the library is working correctly. This is my first real library, and its topic is one that I enjoy a lot.",
-        loc: 3000,
-        time_started: new Date('August 1, 2021 00:00:00'),
-        time_ended: null,
-    },
-    {
-        name: "OpenGL Demos",
-        url: "https://github.com/StappsWorld/openGLDemos",
-        tools: ["rust"],
-        description: "Some <u><strong>OpenGL demos</strong></u> written in Rust. A bit of an on and off collection of graphical projects when I feel like writing something creative.",
-        loc: 30000,
-        time_started: new Date('June 1, 2021 00:00:00'),
-        time_ended: null,
-    },
-    {
-        name: "RHS Mesial API",
-        url: "https://github.com/Riverbend-High-School/rhs-mesial-api",
-        tools: ["python", "rust"],
-        description: "<u><strong>A small API written for the RHS Mesial Project</u></strong> for a volunteer mentorship program at Riverbend Highschool in Fredericksburg, VA. I mentored students in this program. I contributed the code that interfaces with the Google Calendar API to request and parse calendar data to deduce the current block schedule and special events for a specific day.",
-        loc: 1000,
-        time_started: new Date('September 1, 2021 00:00:00'),
-        time_ended: new Date('October 1, 2021 00:00:00'),
-    },
-    {
-        name: "RHS Mesial Dashboard",
-        url: "https://github.com/Riverbend-High-School/rhs-mesial-dashboard",
-        tools: ["vue", "html", "js"],
-        description: "<u><strong>The frontend of the RHS Mesial Project.</u></strong>. This project was written in Vue and displayed information requested from the RHS Mesial API such as the current block, current events, and even special notifications.",
-        loc: 1000,
-        time_started: new Date('September 1, 2021 00:00:00'),
-        time_ended: new Date('October 1, 2021 00:00:00'),
-    },
-    
-]
+  {
+    name: "BRShtop",
+    url: "https://github.com/StappsWorld/brshtop",
+    tools: ["rust"],
+    description:
+      "<u><strong>A Rusty implementation of BpyTop</strong></u>, a stronger, more informative Top written in Python. I ported this program to Rust as a means of immersing myself further the language. It is fully memory safe.",
+    loc: 50000,
+    time_started: new Date("November 1, 2020 00:00:00"),
+    time_ended: new Date("March 1, 2021 00:00:00"),
+  },
+  {
+    name: "YALAL",
+    url: "https://github.com/StappsWorld/YALAL",
+    tools: ["rust"],
+    description:
+      "<u><strong>A simple linear algebra library</strong></u> written in Rust with N dimensional vectors and matrices. Included with the library is unit testing to verify the library is working correctly. This is my first real library, and its topic is one that I enjoy a lot.",
+    loc: 3000,
+    time_started: new Date("August 1, 2021 00:00:00"),
+    time_ended: null,
+  },
+  {
+    name: "OpenGL Demos",
+    url: "https://github.com/StappsWorld/openGLDemos",
+    tools: ["rust"],
+    description:
+      "Some <u><strong>OpenGL demos</strong></u> written in Rust. A bit of an on and off collection of graphical projects when I feel like writing something creative.",
+    loc: 30000,
+    time_started: new Date("June 1, 2021 00:00:00"),
+    time_ended: null,
+  },
+  {
+    name: "RHS Mesial API",
+    url: "https://github.com/Riverbend-High-School/rhs-mesial-api",
+    tools: ["python", "rust"],
+    description:
+      "<u><strong>A small API written for the RHS Mesial Project</u></strong> for a volunteer mentorship program at Riverbend Highschool in Fredericksburg, VA. I mentored students in this program. I contributed the code that interfaces with the Google Calendar API to request and parse calendar data to deduce the current block schedule and special events for a specific day.",
+    loc: 1000,
+    time_started: new Date("September 1, 2021 00:00:00"),
+    time_ended: new Date("October 1, 2021 00:00:00"),
+  },
+  {
+    name: "Gabe Versus Gavin",
+    url: "https://github.com/Riverbend-High-School/gabe_versus_gavin",
+    tools: ["vue", "html", "js", "rust"],
+    description:
+      "<u><strong>A small polling app for a mentor student.</u></strong>. This project was written in Vue and Rust. It allowed a student I mentored to create a poll for the morning news at his school. The students at the school could choose from 1-4 options that this student would create, the backend would record these votes, and the frontend would dynamically update to show the recults.",
+    loc: 2000,
+    time_started: new Date("March 1, 2022 00:00:00"),
+    time_ended: null,
+  },
+];
